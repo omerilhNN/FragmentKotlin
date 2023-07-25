33fad9f97918720fa0b9c79f028c1bb2803483ce
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-07-25 13:21:21 +0300

    Initial commit

---
+++ app/src/main/java/com/omrilhn/fragmentkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.omrilhn.fragmentkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun firstFragment(view:View)
+    {
+
+    }
+    fun secondFragment(view: View)
+    {
+
+    }
+}
